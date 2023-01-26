b41982cce243dc5649115ca9d4e7c5caf531e229
Jaivd7 <42908626+Jaivd7@users.noreply.github.com>
2023-01-26 15:15:15 -0800

    qwerty

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,12 +7,27 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
 	}
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1)); 
+  }
+  
+  @Test
+  public void testReverseinplacenew() {
+    int[] input2 = new int[]{1,2,3,4,5};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{5,4,3,2,1}, input2);
+  }
+
+  @Test
+  public void testReversednew(){
+    int[] input2 = new int[]{1,2,3,4,5};
+    int[] reverse = ArrayExamples.reversed(input2);
+    assertArrayEquals(new int[]{5,4,3,2,1}, reverse);
   }
 }
